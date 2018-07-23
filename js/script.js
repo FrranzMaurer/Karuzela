a53464fbecf9a472268718dd8979ff74d406fca8
Marcin Łukasiewicz <martin.lukasiewicz@gmail.com>
2018-07-23 20:25:15 +0200

    Karuzela mustache działa

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 'use strict';
 (function(){
-
+ 
+var markersOnMap = [];
+var map;
+ 
 var listItems = '';
 var templateItem = document.getElementById('template-product-item').innerHTML;
 Mustache.parse(templateItem);
-
+ 
 for(var i = 0; i < productsData.length; i++){
-	console.log(productsData);
-	listItems += Mustache.render(templateItem, productsData[i]);
+    // console.log(productsData);
+    listItems += Mustache.render(templateItem, productsData[i]);
 }
-
+ 
 var mainCarousel = document.getElementById('main-carousel');
 console.log(listItems, "abc");
 mainCarousel.insertAdjacentHTML('beforeend', listItems);
-
+ 
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( '.main-carousel', {
   // options
@@ -21,18 +24,24 @@ var flkty = new Flickity( '.main-carousel', {
   contain: true,
   hash: true
 });
-
+ 
+flkty.on('settle', function(index){
+        for (var j=0; j < markersOnMap.length; j++) {
+            markersOnMap[j].setMap(j == index ? map : null);
+        }
+});
+ 
 var progressBar = document.querySelector('.progress-bar')
-
+ 
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
-
+ 
 var buttonGroup = document.querySelector('.button-group');
 var buttons = buttonGroup.querySelectorAll('.button');
 buttons = fizzyUIUtils.makeArray( buttons );
-
+ 
 buttonGroup.addEventListener( 'click', function( event ) {
   // filter for button clicks
   if ( !matchesSelector( event.target, '.button' ) ) {
@@ -41,23 +50,35 @@ buttonGroup.addEventListener( 'click', function( event ) {
   var index = buttons.indexOf( event.target );
   flkty.select( index );
 });
-
+ 
 var infos = document.getElementById('infos');
-	
-	// Definiujemy funkcję initMap tak samo jak wcześniej. 
-	
-  	window.initMap = function() {
-
-		var uluru = {lat: -25.363, lng: 131.044};
-		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 4,
-			center: coords
-		});
-		
-		var markerOne = new google.maps.Marker({
-			position: uluru,
-			map: map
-		})};
-
+   
+    // Definiujemy funkcję initMap tak samo jak wcześniej.
+   
+    window.initMap = function() {
+ 
+        var uluru = {lat: -25.363, lng: 131.044};
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+        });
+       
+        var bounds = new google.maps.LatLngBounds();
+ 
+        for(var i = 0; i < productsData.length; i++){
+           
+            bounds.extend(productsData[i].coords);
+            var tmpMarker = new google.maps.Marker({
+                position: productsData[i].coords,
+            });
+       
+            tmpMarker.setMap(i == 0 ? map : null);
+ 
+            markersOnMap.push(tmpMarker);
+        }
+ 
+        map.fitBounds(bounds);
+   
+    };
+ 
 })();
-
